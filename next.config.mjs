3198d36d7bded5408b51dd1537c84706b3553cb5
Tony20100 <tony20100@gmail.com>
2024-08-06 09:36:46 +0700

    host image nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
