629f14b6a917af8ac0c9194b57f9ee2c5b087e12
Tony20100 <tony20100@gmail.com>
2024-08-06 07:44:13 +0700

    2

--- app/identiteNumerique/page.tsx
+++ app/identiteNumerique/page.tsx
@@ -16,25 +16,3 @@ const RedirectPag = () => {
 );
 };
 export default RedirectPag;
-
-
-/*
-
-import React from 'react';
-import HeaderMember from '../../components/headerMember';
-import IdentiteNumerique from '../../components/identiteNumerique';
-
-const IdentiteNumeriquePage = () => {
-  return (
-    <>
-    <div>
-        <HeaderMember />
-        <div className="pt-20">
-            <IdentiteNumerique />
-        </div>
-    </div>
-    </>
-);
-};
-
-export default IdentiteNumeriquePage;*/
