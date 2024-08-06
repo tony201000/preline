8a28d9c8abb09f755ec40ee292b554168682ed6b
Tony20100 <tony20100@gmail.com>
2024-08-06 07:50:56 +0700

    2

--- firebase.ts
+++ firebase.ts
@@ -5,17 +5,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyDpCxVRxvUiskME7iwFVz_6hMcPRnH4apQ",
+  authDomain: "blindboxcollection-6a548.firebaseapp.com",
+  databaseURL: "https://blindboxcollection-6a548-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "blindboxcollection-6a548",
+  storageBucket: "blindboxcollection-6a548.appspot.com",
+  messagingSenderId: "336289874791",
+  appId: "1:336289874791:web:3f4f537bdaeffe9e76a5c2",
+  measurementId: "G-GSNNFF3BWV"
 };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
