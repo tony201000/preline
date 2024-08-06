deba0ee89725affa5598daa86b80add77bf3ea17
Tony20100 <tony20100@gmail.com>
2024-08-06 08:09:46 +0700

    2

--- app/cpf/page.tsx
+++ app/cpf/page.tsx
@@ -5,17 +5,21 @@ import HeaderMember from '../../components/headerMember';
 import FaqCPF from '@/components/faqCpf';
 import ModuleCalCpf from '@/components/modal';
 import HeroCpf from '@/components/heroCpf';
+import ProtectedRoute from '@/components/protectedRoute';
 
 const CPFPage = () => {
   return (
     <>
     <div>
+       
         <HeaderMember />
         <div className="pt-20">
+            <ProtectedRoute>
             <HeroCpf />
             <CPF />
             <FaqCPF />
             <ModuleCalCpf />
+            </ProtectedRoute>
         </div>
     </div>
     </>
--- components/index.tsx
+++ components/index.tsx
@@ -32,21 +32,6 @@ const Index: React.FC = () => {
     return (
         <div className="bg-gradient-to-b from-violet-600/50 via-sky-400" itemScope itemType="https://schema.org/WebPage">
             <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
-                {/* Bannière d&rsquo;annonce */}
-                <div className="flex justify-center">
-                    <a className="group inline-flex items-center bg-white/10 hover:bg-white/10 border border-white/10 p-1 ps-4 rounded-full shadow-md focus:outline-none focus:bg-white/10" href="../figma.html" itemProp="url">
-                        <p className="me-2 text-white text-sm" itemProp="description">
-                            Preline UI Figma est en ligne.
-                        </p>
-                        <span className="group-hover:bg-white/10 py-1.5 px-2.5 flex justify-center items-center gap-x-2 rounded-full bg-white/10 font-semibold text-white text-sm">
-                            <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
-                        </span>
-                    </a>
-                </div>
-                {/* Fin de la bannière d&rsquo;annonce */}
-
                 {/* Titre */}
                 <div className="max-w-3xl text-center mx-auto">
                     <h1 className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl" itemProp="headline">
