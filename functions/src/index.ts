427c07ce9881b2f64cac962b18a6309023a74fbe
Tony20100 <tony20100@gmail.com>
2024-08-09 21:18:59 +0700

    55

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,40 @@
-// * Import function triggers from their respective submodules:
-// *
-// * import {onCall} from "firebase-functions/v2/https";
-// * import {onDocumentWritten} from "firebase-functions/v2/firestore";
-// *
-// * See a full list of supported triggers at https://firebase.google.com/docs/functions
-//
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-//
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import { logger } from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+export const addmessage = onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original: string = req.query.text as string;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+      .collection("messages")
+      .add({ original });
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
+
+// Listens for new messages added to /messages/:documentId/original
+// and saves an uppercased version of the message
+// to /messages/:documentId/uppercase
+export const makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
+  // Grab the current value of what was written to Firestore.
+  const original: string = event.data?.data()?.original || '';
+
+  // Access the parameter `{documentId}` with `event.params`
+  logger.log("Uppercasing", event.params.documentId, original);
+
+  const uppercase: string = original.toUpperCase();
+
+  // You must return a Promise when performing
+  // asynchronous tasks inside a function
+  // such as writing to Firestore.
+  // Setting an 'uppercase' field in Firestore document returns a Promise.
+  return event.data?.ref.set({ uppercase }, { merge: true });
+});
+
